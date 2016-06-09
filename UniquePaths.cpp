59acfc3a6d775aebee56be054d120d9a6f55c244
DuanJingjing <canjiandjj@163.com>
2016-06-09 22:10:03 +0800

    UniquePaths.cpp

---
+++ UniquePaths.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+int uniquePaths(int m, int n)
+{
+    if(m <1 || n<1) return 0;
+
+    int dp[m][n];
+    memset(dp, 0, sizeof(int)*m*n);
+
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++) {
+            if(i==0 || j==0) {
+                dp[i][j]=1;
+            }
+            else {
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+    }
+
+    return dp[m-1][n-1];
+}
+
+int main(int argc, char* argv[])
+{
+    int m=3, n=7;
+    cout << uniquePaths(m,n)<<endl;
+}
